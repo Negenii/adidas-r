ac71b2137a8dc5f1e48d90e86510a6211599e145
Evgeny Ugreninov <ikra.ad@mac.com>
2017-05-16 20:30:40 +0300

    Added filter buttons

--- src/Components/Products/List/index.jsx
+++ src/Components/Products/List/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib';
 import Card from './card';
+import Filters from './Filters';
 
 const Home = () => (
   <Grid fluid>
+    <Row>
+      <Col xs={12} md={12} lg={12}>
+        <Filters />
+      </Col>
+    </Row>
     <Row>
       <Col xs={12} md={4} lg={4}>
         <Card />
